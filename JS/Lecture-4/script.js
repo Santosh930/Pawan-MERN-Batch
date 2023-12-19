ebb148c9f62bb0e107f5267dfe26fdab1bb9bfdb
santosh <santoshtajpur@gmail.com>
2023-12-19 12:17:09 +0530

    Lecture-4 Operators

---
+++ JS/Lecture-4/script.js
@@ -0,0 +1,220 @@
+// console.log("Lecture-4");
+
+
+// Operator
+
+
+// 1.Binary Number(Base-2(0,1));
+
+/* 
+
+168421
+1-0001
+2-0010
+3-0011
+4-0100
+
+15-1111
+
+
+
+2.Decimal(base-10(0,1....9));
+
+3.Octal Number(base-8(0,1...7));
+4.Hexadecimal(base-16(0,1....9,A,B,C,D,E,F));
+
+
+OR truth Table
+
+X Y Z
+0 1 1
+1 0 1
+0 0 0
+1 1 1
+
+
+
+AND Truth Table
+
+X Y Z
+0 1 0
+1 0 0
+0 0 0
+1 1 1
+
+
+
+
+
+
+
+
+
+*/
+
+
+//Arithmetic operators
+
+// let a=20;
+// let b=30;
+
+// console.log(a+b);//50
+// console.log(a-b);//-10
+// console.log(a*b);//600
+// console.log(a/b);//
+
+
+// console.log(7%2);//1
+
+// let x=13;
+// let y=12;
+
+// console.log(x%2==0);//false
+// console.log(y%2==0);//true
+
+
+// console.log(2+ '123');//2123
+// console.log(2 -"2");//22
+// console.log(1+true);//2
+// console.log(1-true);//0
+
+// let  a=20;
+//  console.log(a++);
+//  console.log(a);
+// let  b=20; 
+// console.log(++b);
+
+
+//2 Comparison operatoers
+
+// let a=10;
+// let b='10';
+// console.log(typeof a);//number
+// console.log(typeof b);//String
+// console.log(a==b);//true
+// console.log(a===b);//false
+
+
+
+// let a=20;
+// let b=10;
+
+// console.log(a<b);//false
+// console.log(a>b);//true
+// console.log(a<=b);//false
+// console.log(a>=b);//true
+// console.log(a!=b);//true
+// console.log(a!==b);//true
+// // console.log(a<b);
+
+
+
+//3 Logical operator
+
+// console.log(true && false);//false;
+// console.log(true && true);//false;
+
+
+// Logical Or
+
+// console.log(false || false);//false
+// console.log(true || false);//true;
+
+// console.log(1==10  && console.log('Pawan'));
+
+
+
+//Assignment operator
+
+// let sum=0;
+// let mul=1;
+// let sub=60;
+
+// for(let i=1;i<=10;i++){
+//     // sum += i;
+//     sum =sum+i;
+//     mul *= i;
+//     sub -= i;
+// }
+// console.log(sum);
+// console.log(sub);
+// console.log(mul);
+
+
+
+
+//ternary operator
+// let a=10;
+
+
+// if(a==10){
+//     console.log(`The Number is  equal to ${a}`);
+//     console.log("The Number is  equal to",a);
+// }
+// else{
+//     console.log(2);
+// }
+
+// a==12?console.log(`The Number is  equal to ${a}`):console.log("The Number is not  equal to",a)
+
+
+// let obj={
+//     name:'Pawan',
+//     city:'Delhi',
+//     mob:9873481283,
+//     email:'pawan@gmail.com'
+// }
+
+// for(var key in obj){
+//     console.log(key);
+//     console.log(obj[key]);
+// }
+// var ans=delete(obj.name);
+// console.log(obj.name);
+
+
+
+// var let const
+
+// var x=10;
+// var x=20;
+// var x=30;
+// let y=10;
+//  y=20;
+//  const z=10;
+//   z=20;
+
+
+
+// console.log(x);
+// var x=20;
+
+// console.log(x);
+
+// let c=30;
+// function add(){
+//     let a=10;
+//     let b=20;
+//     console.log(a+b);
+// }
+
+// console.log(a);
+// console.log(b);
+// add();
+
+//Bitwise operators
+
+
+// console.log(3^4);//7
+
+/* 
+0011
+0100
+0111 
+
+
+
+*/
+
+// console.log(~10);//-11
+// console.log(~-10);//9
