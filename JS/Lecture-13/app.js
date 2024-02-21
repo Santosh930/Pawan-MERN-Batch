569c8cbc858b46fd29d096c3c6e42f520ac777d0
santosh <santoshtajpur@gmail.com>
2024-02-21 12:18:03 +0530

    Lecture-13 uploaded

--- JS/Lecture-13/app.js
+++ JS/Lecture-13/app.js
@@ -1,2 +1,90 @@
-console.log("Hello Santosh Kumar");
+// console.log("Hello Santosh Kumar");
+//promise/fetch/async/await/
+
+
+// console.log(1)
+// console.log(2)
+// for(let i=0;i<=99999;i++){
+//     if(i==9999){
+//         console.log(i);
+//     }
+
+// }
+
+// setTimeout(()=>{
+//     console.log("Inside settimeout");
+// },5000);
+// console.log(3)
+// console.log(4)
+// console.log(5)
+
+//promise
+
+// let myPromise=new Promise(function(res,rej){
+//     let x=2;
+
+//     if(x==2){
+
+//         res(()=>{
+//             console.log("The promise is resolved");
+//         });
+
+//     }
+//     else{
+//         rej(()=>{
+//             console.log("This is rejected!");
+//         });
+//     }
+    
+   
+// });
+
+
+// let myPromise=new Promise(function(res,rej){
+//     res();
+//     rej();
+// });
+
+
+// console.log(myPromise);
+
+// myPromise.then(
+
+    
+// function(value){
+//     console.log(123);
+// },
+// function(err){
+//     console.log(err);
+// }
+
+// )
+// let ans=fetch('https://fakestoreapi.com/products');
+// console.log(ans);
+
+
+// let ans=(res)=>{
+//     console.log(res);
+// }
+
+// let ans=res=>console.log(res);
+    
+
+
+// ans(1234);
+
+// fetch('https://fakestoreapi.com/products')
+//             .then(res=>res.json())
+//             .then(json=>console.log(json))
+
+
+// async function getData(){
+//     let res= await fetch('https://fakestoreapi.com/products');
+//     let data=await res.json();
+//     console.log(data);
+// };
+
+// getData();
+
+
 
